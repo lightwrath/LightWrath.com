c0aa8c648e9437d6727e15ca02736cb109f8cfe7
lightwrath <lightwrath@live.com>
2020-01-15 00:41:43 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 function showProjects() {
     document.getElementById("projects").style.display = "block";
     document.getElementById("resume").style.display = "none";
+    document.getElementById("about").style.display = "none";
     document.getElementById("contact").style.display = "none";
 }
+
 function showResume() {
     document.getElementById("projects").style.display = "none";
     document.getElementById("resume").style.display = "block";
+    document.getElementById("about").style.display = "none";
     document.getElementById("contact").style.display = "none";
 }
+
+function showAbout() {
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("resume").style.display = "none";
+    document.getElementById("about").style.display = "block";
+    document.getElementById("contact").style.display = "none";
+}
+
 function showContact() {
     document.getElementById("projects").style.display = "none";
     document.getElementById("resume").style.display = "none";
+    document.getElementById("about").style.display = "none";
     document.getElementById("contact").style.display = "block";
 }
